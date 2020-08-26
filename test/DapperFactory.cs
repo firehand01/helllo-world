0642b7f79bbb4d9f07ec2a56c2c86e57cedc1da5
firehand01 <firehand01@163.com>
2020-08-26 10:09:16 +0800

    添加文件夹

---
+++ test/DapperFactory.cs
@@ -0,0 +1,17 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WinCrud.Orm
+{
+    public class DapperFactory
+    {
+        public static OracleConnection CrateOracleConnection(string connectionString)
+        {
+            var connection = new OracleConnection(connectionString);
+            return connection;
+        }
+    }
+}
